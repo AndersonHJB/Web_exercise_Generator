7ed5609770de8f150e8b062ce51898d24f54fd32
AndersonHJB <bornforthis@bornforthis.cn>
2022-07-14 18:23:17 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Web exercise Generator
 
+- 文件名称
+- 测试的 Web 代码
+- 输出路径
+- 返回路径
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
 class Web_exercise_Generator():
 	def read(self):
 		with open("./template_html/template_html.html", "r", encoding="utf-8")as f:
-			print(f.read())
-
+			# print(f.read())
+			return f.read()
+	def generator(self, html, question):
+		html = html.format(content=question)
+		with open("")
+	
 if __name__ == '__main__':
 	web = Web_exercise_Generator()
-	web.read()
+	html = web.read()
+	web.generator(html)
